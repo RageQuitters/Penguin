f4e835ded713e278cb7133bf173767c139277846
RageQuitters <zacharyongzeen@gmail.com>
2026-03-23 20:16:32 +0800

    added mock ups

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,8 @@ df = load_dataset("dataset/train_test.csv")
 df_clean = preprocess_dataset(df)
 
 # Comment out after training models successfully once.
-train_fault_model(df_clean)
-train_urgency_model(df_clean)
+#train_fault_model(df_clean)
+#train_urgency_model(df_clean)
 
-print(predict_new_point((290.0, 30.0, 2500, 4, 5)))
+print(predict_new_point((290.0, 30.0, 1500, 4, 1)))
 print(predict_new_point((290.0, 30.0, 2500, 4, 6)))
